5dc7e80bb76987aa6b2e3c73207f535604034e32
Дмитрий Беликов <dmtrybelickov@gmail.com>
2023-06-07 21:55:31 +0300

    cloud work prep

--- README.md
+++ README.md
@@ -1,1 +1,36 @@
-# praktikum_new_diplom
+# Foodgram  
+Simple web-app to exchange cooking recipes with your friends. Find, like and create shopping list for your lunch just in few clicks.  
+  
+  
+### Stack:  
+Backend based mostly on next technologies:  
+- Python  
+- Django  
+- DRF  
+- Djoser  
+- Docker  
+- PostgreSQL  
+- nginx  
+- gunicorn  
+
+Front-end based on React and made by Ya.Practicum team.  
+
+
+
+### How-to:  
+Download ```infra``` folder from repo and run ```$ docker-compose up``` in ```infra``` folder.   
+Don't forget to create ```.env``` file and populate it with Django Database settings.  
+Run ```docker-compose exec backend bash``` and then   
+```
+python3 manage.py makemigrations && python3 manage.py migrate && python3 manage.py collectstatic && python3 manage.py load_json ingredients.json && python3 manage.py createsuperuser  
+```  
+
+### API
+Documentations with example requests and responses availiable at   
+51.250.69.189/api/docs/redoc  
+  
+### Author:
+Dmitriy Belikov
+
+### Check it out at...  
+51.250.69.189/
--- backend/foodgram/settings.py
+++ backend/foodgram/settings.py
@@ -60,23 +60,23 @@ TEMPLATES = [
 
 WSGI_APPLICATION = 'foodgram.wsgi.application'
 
-# DATABASES = {
-#     'default': {
-#         'ENGINE': os.getenv('DB_ENGINE', 'django.db.backends.postgresql'),
-#         'NAME': os.getenv('DB_NAME', 'postgres'),
-#         'USER': os.getenv('POSTGRES_USER', 'postgres'),
-#         'PASSWORD': os.getenv('POSTGRES_PASSWORD', 'postgres'),
-#         'HOST': os.getenv('DB_HOST', 'db'),
-#         'PORT': os.getenv('DB_PORT', '5432')
-#     }
-# }
-
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': os.getenv('DB_ENGINE', 'django.db.backends.postgresql'),
+        'NAME': os.getenv('DB_NAME', 'postgres'),
+        'USER': os.getenv('POSTGRES_USER', 'postgres'),
+        'PASSWORD': os.getenv('POSTGRES_PASSWORD', 'postgres'),
+        'HOST': os.getenv('DB_HOST', 'db'),
+        'PORT': os.getenv('DB_PORT', '5432')
     }
 }
+
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+#     }
+# }
 
 
 DJOSER = {
